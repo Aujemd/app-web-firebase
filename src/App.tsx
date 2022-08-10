e48eb93bd87bbaf0d3f0f0d6a964efe2db96bce3
Carlos Rosales <carlosros033@gmail.com>
2022-08-09 22:26:31 -0400

    feature(data): fetching data and formatting it from the firebase db

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,58 @@
 import { useEffect } from "react";
 import { MainTitle } from "./Components/MainTitle";
-import { getTime, db } from "./firebase";
+import {
+  getLastTemperatures,
+  getLastYears,
+  getLastMonths,
+  getLastDays,
+  getLastMinutes,
+  getLastHours,
+} from "./firebase";
+
+const formatData = (
+  temperatures: number[],
+  years: number[],
+  month: number[],
+  days: number[],
+  hours: number[],
+  minutes: number[]
+) => {
+  const datesArray = years.map((_, i) => {
+    return new Date(years[i], month[i], days[i], hours[i], minutes[i], 0, 0);
+  });
+
+  return datesArray.map((date, i) => [date, temperatures[i]]);
+};
 
 const App = (): JSX.Element => {
   useEffect(() => {
-    const fetchTime = async () => {
+    const fetchData = async () => {
       try {
-        const timeResult = await getTime(db);
-        console.log(timeResult);
+        const temperatures = await getLastTemperatures();
+        const years = await getLastYears();
+        const months = await getLastMonths();
+        const days = await getLastDays();
+        const hours = await getLastHours();
+        const minutes = await getLastMinutes();
+
+        const formattedData = formatData(
+          temperatures,
+          years,
+          months,
+          days,
+          hours,
+          minutes
+        );
+
+        console.log("[date, temperature] = ", formattedData);
+
+        return formattedData;
       } catch (e) {
         console.error(e);
       }
     };
 
-    fetchTime();
+    fetchData();
   }, []);
 
   return (
--- src/firebase/fetchers.ts
+++ src/firebase/fetchers.ts
@@ -1,8 +1,46 @@
-import { collection, Firestore, getDocs } from "firebase/firestore/lite";
+import { getDatabase, ref, onValue } from "firebase/database";
 
-export const getTime = async (db: Firestore) => {
-  const timeCol = collection(db, "time");
-  const timeSnapshot = await getDocs(timeCol);
-  const timeList = timeSnapshot.docs.map((doc) => doc.data());
-  return timeList;
+const refNamesObject = {
+  temperature: "Nodemcu/TThermok",
+  year: "Nodemcu/Ano",
+  month: "Nodemcu/Mes",
+  day: "Nodemcu/Dia",
+  hour: "Nodemcu/Hora",
+  minute: "Nodemcu/Minutos",
+};
+
+const getValues = async (refName: string, qty = 80): Promise<number[]> => {
+  const db = getDatabase();
+  const tempRef = ref(db, refName);
+  let values: number[] = [];
+  onValue(tempRef, (snapshot) => {
+    const data = snapshot.val();
+    values = Object.values(data).slice(0, qty) as number[];
+  });
+
+  return values;
+};
+
+export const getLastTemperatures = async (quantity = 80) => {
+  return getValues(refNamesObject["temperature"], quantity);
+};
+
+export const getLastYears = async (quantity = 80) => {
+  return getValues(refNamesObject["year"], quantity);
+};
+
+export const getLastMonths = async (quantity = 80) => {
+  return getValues(refNamesObject["month"], quantity);
+};
+
+export const getLastDays = async (quantity = 80) => {
+  return getValues(refNamesObject["day"], quantity);
+};
+
+export const getLastHours = async (quantity = 80) => {
+  return getValues(refNamesObject["hour"], quantity);
+};
+
+export const getLastMinutes = async (quantity = 80) => {
+  return getValues(refNamesObject["minute"], quantity);
 };
