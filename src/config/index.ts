f92b68a7683efbdf6993933cac7987687ca0b2c3
Carlos Rosales <carlosros033@gmail.com>
2022-08-09 20:59:46 -0400

    chore(firebase): adding firebase package to the project

---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig: Record<string, string> = {
+  apiKey: "AIzaSyDtXOvG9tmXRYjDD7O3EYjzz2syivlbTAA",
+  authDomain: "horno-panadero.firebaseapp.com",
+  databaseURL: "https://horno-panadero-default-rtdb.firebaseio.com",
+  projectId: "horno-panadero",
+  storageBucket: "horno-panadero.appspot.com",
+  messagingSenderId: "310370306205",
+  appId: "1:310370306205:web:64ff344cc779b97dffac74",
+};
