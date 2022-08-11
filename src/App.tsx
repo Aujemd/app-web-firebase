1341412fcf959e41c76b776e427771bb86821865
Johan <joenmodu99@gmail.com>
2022-08-10 23:45:40 -0400

    feat(display): Display of data from table and chart added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { useEffect, useState } from "react";
-import { MainTitle } from "./Components/MainTitle";
+import { useEffect, useState } from 'react'
+import { MainTitle } from './Components/MainTitle'
 import {
   getLastTemperatures,
   getLastYears,
   getLastMonths,
   getLastDays,
   getLastMinutes,
-  getLastHours,
-} from "./firebase";
+  getLastHours
+} from './firebase'
+
+import { ProgressSpinner } from 'primereact/progressspinner'
+import { DataTable } from 'primereact/datatable'
+import { Column } from 'primereact/column'
+import { Chart } from 'primereact/chart'
 
 const formatData = (
   temperatures: number[],
@@ -18,42 +23,48 @@ const formatData = (
   minutes: number[]
 ) => {
   const datesArray = years.map((_, i) => {
-    return new Date(years[i], month[i], days[i], hours[i], minutes[i], 0, 0);
-  });
+    return new Date(years[i], month[i], days[i], hours[i], minutes[i], 0, 0)
+  })
 
-  return datesArray.map((date, i) => [date, temperatures[i]]);
-};
+  return datesArray.map((date, i) => {
+    return {
+      date: date.toLocaleString('es-VE'),
+      temperature: temperatures[i]
+    }
+  })
+}
 
+interface IData {
+  date: string
+  temperature: number
+}
 const App = (): JSX.Element => {
-  const [loading, setLoading] = useState<boolean>(true);
-  const [temperatures, setTemperatures] = useState<number[]>([]);
-  const [years, setYears] = useState<number[]>([]);
-  const [months, setMonths] = useState<number[]>([]);
-  const [days, setDays] = useState<number[]>([]);
-  const [hours, setHours] = useState<number[]>([]);
-  const [minutes, setMinutes] = useState<number[]>([]);
-
-  useEffect(() => {
-    console.log("loading = ", loading);
-  }, [loading]);
-
+  const [loading, setLoading] = useState<boolean>(true)
+  const [temperatures, setTemperatures] = useState<number[]>([])
+  const [years, setYears] = useState<number[]>([])
+  const [months, setMonths] = useState<number[]>([])
+  const [days, setDays] = useState<number[]>([])
+  const [hours, setHours] = useState<number[]>([])
+  const [minutes, setMinutes] = useState<number[]>([])
+  const [data, setData] = useState<Array<IData>>([])
+  const [charData, setCharData] = useState({})
   useEffect(() => {
     const initializeDataFetcher = async () => {
       try {
-        setLoading(true);
-        await getLastTemperatures(setTemperatures);
-        await getLastYears(setYears);
-        await getLastMonths(setMonths);
-        await getLastDays(setDays);
-        await getLastHours(setHours);
-        await getLastMinutes(setMinutes);
+        setLoading(true)
+        await getLastTemperatures(setTemperatures)
+        await getLastYears(setYears)
+        await getLastMonths(setMonths)
+        await getLastDays(setDays)
+        await getLastHours(setHours)
+        await getLastMinutes(setMinutes)
       } catch (e) {
-        console.error(e);
+        console.error(e)
       }
-    };
+    }
 
-    initializeDataFetcher();
-  }, []);
+    initializeDataFetcher()
+  }, [])
 
   useEffect(() => {
     if (
@@ -64,24 +75,62 @@ const App = (): JSX.Element => {
       hours.length &&
       minutes.length
     ) {
-      const formattedData = formatData(
-        temperatures,
-        years,
-        months,
-        days,
-        hours,
-        minutes
-      );
-      console.log("[date, temperature] = ", formattedData);
-      setLoading(false);
+      setData(() => [...formatData(temperatures, years, months, days, hours, minutes)])
+      setLoading(false)
     }
-  }, [temperatures, years, months, days, hours, minutes]);
+  }, [temperatures, years, months, days, hours, minutes])
 
+  useEffect(() => {
+    const parsedData = data.slice(-6)
+    console.log(parsedData)
+
+    if (parsedData) {
+      setCharData(() => {
+        return {
+          labels: parsedData.map((item) => item.date),
+          datasets: [
+            {
+              label: 'Temperatura',
+              data: parsedData.map((item) => item.temperature),
+              fill: false,
+              borderColor: '#FF6D32',
+              tension: 0.4
+            }
+          ]
+        }
+      })
+    }
+  }, [data])
   return (
     <main>
       <MainTitle />
-    </main>
-  );
-};
 
-export default App;
+      {loading ? (
+        <div className='container-spinner'>
+          <ProgressSpinner />
+        </div>
+      ) : (
+        <>
+          <DataTable
+            style={{ marginBottom: '100px' }}
+            responsiveLayout='scroll'
+            value={data}
+            stripedRows
+            scrollable
+            scrollHeight='400px'
+            showGridlines
+          >
+            <Column field='date' header='Fecha' sortable></Column>
+            <Column field='temperature' header='Temperatura (°C)' sortable></Column>
+          </DataTable>
+          <h1 className='dashboard-main-title'>
+            Comportamiento de los ultimos registros de temperatura
+          </h1>
+          <Chart type='line' data={charData} style={{ background: '#fff' }} />
+        </>
+      )}
+    </main>
+  )
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+import 'primereact/resources/themes/lara-light-indigo/theme.css' //theme
+import 'primereact/resources/primereact.min.css' //core css
+import 'primeicons/primeicons.css' //icons
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
